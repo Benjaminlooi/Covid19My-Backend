98665f58cbbe0c7464250a3283b63f637f4e7c8f
Benjamin Looi <benjaminlooidt@gmail.com>
2020-03-23 21:06:10 +0800

    Added CORS

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const { getCovid19MyCases } = require("./scrapper")
 const app = express();
 const port = 9000
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 app.get('/', async (req, res) => {
   res.send(await getCovid19MyCases())
 })
