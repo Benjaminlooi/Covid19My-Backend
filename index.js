2ecb73ca8bf3f7d7ef6e11e8518025562e40fd8d
Benjaminlooi <benjaminlooidt@gmail.com>
2020-06-19 11:12:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { outbreakMyScrapper } = require("./scrapper")
 
 const app = express();
-const port = 9000
+const port = process.env.PORT || 9000
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
